67a0beb94124728924ccc12584f44239c0cebf28
Kenny <kenny02238@yahoo.com.tw>
2024-01-05 16:20:15 +0800

    feat: add sidebar recommend / skeleton / live btn / avatar

---
+++ components/hint.tsx
@@ -0,0 +1,33 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface HintProps {
+  label: string;
+  children: React.ReactNode;
+  side?: "left" | "right" | "top" | "bottom";
+  align?: "start" | "center" | "end";
+  asChild?: boolean;
+}
+
+export const Hint = ({ asChild, children, label, side, align }: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger asChild={asChild}>
+          <div>{children}</div>
+        </TooltipTrigger>
+        <TooltipContent
+          className="text-black bg-white"
+          side={side}
+          align={align}
+        >
+          <p className="font-semibold">{label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
